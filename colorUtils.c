32dfe1ce5863607f878d54283d16dbe393d4234c
bilalmalik101 <144342543+bilalmalik101@users.noreply.github.com>
2023-12-19 19:13:28 +0500

    using lightness tachnique for making function

--- colorUtils.c
+++ colorUtils.c
@@ -13,5 +13,14 @@ int max(int x, int y, int z) {
 
 int toGrayScaleAverage(int r, int g, int b) {
   //TODO: test this, it may be wrong!
+
   return (r + g + b) / 3;
+
+}
+// using lightness technique from read.me
+int toGrayScalelightness(int r, int g, int b){
+  int max = 0 , min = 0;
+  if(r>g && r>b){
+    max = r;
+  }
 }
